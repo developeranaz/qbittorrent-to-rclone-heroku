73a16cfcd1f5c0eb9301241881c63a28453419b2
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-26 23:01:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl "$RCLONE_CONFIG_IN_RAW_URL" >$(rclone config file |grep 'rclone.conf')
 echo $PORT >/PORT
+curl "$QBITTORRENT_CONFIG_IN_RAW_URL" >/root/qbittorrent/qBittorrent.conf
