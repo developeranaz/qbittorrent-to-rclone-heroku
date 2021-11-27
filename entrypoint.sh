c0271ee306ec13e391c0b32b4e4b8d829c3c6ed2
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-27 16:42:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,8 @@
 curl -L $RCLONE_CONFIG_IN_RAW_URL >/root/.config/rclone/rclone.conf
 echo $PORT >/PORT
 curl -L $QBITTORRENT_CONFIG_IN_RAW_URL >/root/.config/qBittorrent/qBittorrent.conf
+while :
+do
 jupyter notebook --ip=0.0.0.0 --port="$(cat /PORT)" --NotebookApp.token='' --NotebookApp.password=''
 qbittorrent-nox --webui-port="$(cat /PORT)"
+done
