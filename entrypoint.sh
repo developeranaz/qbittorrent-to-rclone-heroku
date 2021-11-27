009020d312421954b1c30816bc0836b656e601e0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-27 18:10:34 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 curl -L $RCLONE_CONFIG_IN_RAW_URL >/root/.config/rclone/rclone.conf
 echo $PORT >/PORT
 #curl -L $QBITTORRENT_CONFIG_IN_RAW_URL >/qBittorrent/config/qBittorrent.conf
+rclone listremotes
 while :
 do
 qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)"
