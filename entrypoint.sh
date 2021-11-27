7fbbc8cdf24b7e4a06d0a627fed31452644c248f
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-27 21:10:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ curl -L $RCLONE_CONFIG_IN_RAW_URL >/.config/rclone/rclone.conf
 echo $PORT >/PORT
 #curl -L $QBITTORRENT_CONFIG_IN_RAW_URL
 cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
-echo $WEBPASSWORD
+echo $WEBPASSWORD |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
 WEBHASHPASSWORD=
-cat /qBittorrent/qBittorrent.conf |sed "s|devanaz|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSERNAME|g" |sed "s||$WEBHASHPASSWORD|g" >/qBittorrent/config/qBittorrent.conf
+cat /qBittorrent/qBittorrent.conf |sed "s|devanaz|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSERNAME|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
 while :
 do
 qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)"
