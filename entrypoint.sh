5e3d9b59671a022bc1148a0b30940470753af0cb
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-28 01:05:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Scripted By DevAnaZ"
+echo "Orgin repository : https://github.com/developeranaz/qbittorrent-to-rclone-heroku"
 mkdir /.config
 mkdir /.config/rclone
 curl -L $RCLONE_CONFIG_IN_RAW_URL >/.config/rclone/rclone.conf
@@ -6,7 +8,6 @@ echo $PORT >/PORT
 #curl -L $QBITTORRENT_CONFIG_IN_RAW_URL
 cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
 echo $WEBPASSWORD |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
-WEBHASHPASSWORD=
 cat /qBittorrent/qBittorrent.conf |sed "s|devanaz|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSERNAME|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
 while :
 do
