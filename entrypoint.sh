c9b72edc6a9b628cc213ab528cbe1ac88cc606b0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-28 07:24:37 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,19 @@ echo $PORT >/PORT
 cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
 echo $WEBPASSWORD |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
 cat /qBittorrent/qBittorrent.conf |sed "s|devanaz|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSERNAME|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
+echo 'retaining previous files....' >index.html
+log="index.html"
+rclone rcd --rc-serve --rc-addr=0.0.0.0:$(/PORT) --rc-template=/index.html & rclone copy devanaz:qbit /qBittorrent --progress > "$log" 2>&1 &
+pid=rclone
+
+while sleep 1
+do
+    if fgrep --quiet "$match" "$log"
+    then
+        kill $pid
+        exit 0
+    fi
+done
 while :
 do
 qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)"
