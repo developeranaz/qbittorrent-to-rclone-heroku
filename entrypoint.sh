15a0a4df051b86f0ecbe777d42005f10d79adba6
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-29 18:08:32 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ mkdir /.config/rclone
 curl -L $RCLONE_CONFIG_IN_RAW_URL >/.config/rclone/rclone.conf
 echo $PORT >/PORT
 #curl -L $QBITTORRENT_CONFIG_IN_RAW_URL
-cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
-echo $WEBPASSWORD |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
+#cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
+#echo $WEBPASSWORD |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
 #cat /qBittorrent/qBittorrent.conf |sed "s|devanaz|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSERNAME|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
-echo 'retaining previous files....' >index.html
+#echo 'retaining previous files....' >index.html
 log="index.html"
-sleep 10; qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)" & rclone copy devanaz:qbit/temp /qBittorrent --progress 
+sleep 15; qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)" & rclone copy devanaz:qbit/temp /qBittorrent --progress 
 pid=rclone
 match=', 100%'
 while sleep 10
