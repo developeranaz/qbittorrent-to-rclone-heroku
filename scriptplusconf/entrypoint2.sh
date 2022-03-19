9a9f5d31292186a9a36ad8a8ae531b7f28ec3575
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-19 09:38:09 +0530

    Add files via upload

---
+++ scriptplusconf/entrypoint2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+XATH=/scriptplusconf
+#SELFPING='newapp'
+
+echo "$selfping $Selfping $SelfPing $SELFPING" >$XATH/selfping
+if grep -i "[A-Za-z]" $XATH/selfping
+then
+    echo "found"
+    rawping=$( cat $XATH/selfping |sed 's/ //g' |grep [A-Za-z] |sed 's/^https:\/\/\|^http:\/\///g' |sed 's/.herokuapp.com//g' |sed 's@^@https://@g' |sed 's@$@.herokuapp.com/ping@g')
+    echo "$rawping"
+    sed -i "s|SELFPINGURL|$rawping|g" $XATH/selfping.sh
+    chmod +x $XATH/selfping.sh
+    sed -i "s@\$XATH@$XATH@g" /scriptplusconf/supervisord.conf
+    supervisord -c /scriptplusconf/supervisord.conf
+else
+    echo "No selfping url or appname found, hence not using supervisord"
+    echo "You can add selfping by adding a env var in heroku, SELFPING and correspondingly its value as your appname or appurl"
+    echo "Note: 'SELFPING' uses more RAM and takes up a lot of free dynos, and your app will stop working when your allocated dyno hours are exceeded."
+    mkdir /.config
+    mkdir /.config/rclone
+    curl -L $RCLONE_CONFIG_IN_RAW_URL >/.config/rclone/rclone.conf
+    echo $PORT >/PORT
+    cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
+    echo -n "$WEBPASSWORD" |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
+    cat /qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
+
+    while :
+    do
+       qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)"
+    done
+fi
---
+++ scriptplusconf/selfping.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while :
+do
+sleep 1440
+curl -L "SELFPINGURL"
+done
