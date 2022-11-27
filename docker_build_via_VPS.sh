9df82fd4c7c3cf47c93868836906bf9cdb901a65
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-11-27 19:59:37 +0530

    Update docker_build_via_VPS.sh

--- docker_build_via_VPS.sh
+++ docker_build_via_VPS.sh
@@ -1,1 +1,38 @@
+#!/bin/bash
 
+# Created by DevAnaZ
+# run example below
+# docker_build_via_VPS.sh -c 8080 -p newpassword -u newusername
+
+
+while getopts ":u:p:c:" opt; do
+  case $opt in
+    u) username="$OPTARG"
+    ;;
+    p) password="$OPTARG"
+    ;;
+    c) port="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+
+
+
+cd
+apt install git -y
+rm -r .devanaztempinstall
+mkdir .devanaztempinstall
+cd .devanaztempinstall
+git clone https://github.com/developeranaz/qbittorrent-to-rclone-heroku
+cd qbittorrent-to-rclone-heroku
+
+docker build .
+
+docker images |head -2| tail -1 |sed 's/ /\n/g' |sed '/^$/d' |head -3| tail -1 >imagename
+
+cd
+rm -r .devanaztempinstall
+docker run -td -e username="$username" -e password="$password" -p $port:8880 "$(docker images |head -2| tail -1 |sed 's/ /\n/g' |sed '/^$/d' |head -3| tail -1)"
